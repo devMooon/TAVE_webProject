68ab4f8773bf2b41f6d23e4d09fdef8c1a470451
Seo-Yeon Mun <anstjdus0702@duksung.ac.kr>
2021-09-22 18:38:09 +0900

    데이터 불러오기 전

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -4,17 +4,34 @@
       <a v-for="(a, i) in menus" :key="i">{{a}}</a>
     </div>
 
+    <div class="black-bg" v-if="모달창열렸니 == true">
+      <div class="white-bg">
+        <h4>상세페이지임</h4>
+        <p>상세페이지 내용임</p>
+        <button @click="모달창열렸니 = false">닫기</button>
+      </div>
+    </div>
+    
+
+
     <div>
-      <h4>{{products[0]}}</h4>
+      <img src="./assets/room0.jpg" class="room-img">
+      <h4 @click="모달창열렸니 = true">{{products[0]}}</h4>
       <p>{{price[0]}} 만원</p>
       <button @click="increase(0)">허위매물신고</button> <span>신고수 : {{신고수[0]}}</span>
     </div>
+
+  
     <div>
+      <img src="./assets/room1.jpg" class="room-img">
       <h4>{{products[1]}}</h4>
       <p>{{price[1]}} 만원</p>
       <button @click="increase(1)">허위매물신고</button> <span>신고수 : {{신고수[1]}}</span>
     </div>
+
+    
     <div>
+      <img src="./assets/room2.jpg" class="room-img">
       <h4>{{products[2]}}</h4>
       <p>{{price[2]}} 만원</p>
       <button @click="increase(2)">허위매물신고</button> <span>신고수 : {{신고수[2]}}</span>
@@ -23,10 +40,14 @@
 </template>
 
 <script>
+import data from './assets/oneroom.js';
+
 export default {
   name: 'App',
   data() {
     return{
+      원룸들 : data,
+      모달창열렸니 : false,
       신고수 : [0, 0, 0],
       menus : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
@@ -44,6 +65,23 @@ export default {
 </script>
 
 <style>
+body {
+  margin : 0
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height:100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -61,5 +99,10 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
 }
 </style>
