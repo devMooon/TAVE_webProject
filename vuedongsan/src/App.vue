20aba272da3ea045f1df37e6caf5e125d5520077
Seo-Yeon Mun <anstjdus0702@duksung.ac.kr>
2021-09-22 18:46:22 +0900

    데이터 불러오기
    코딩애플 vue 무료강의 끝

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -14,27 +14,10 @@
     
 
 
-    <div>
-      <img src="./assets/room0.jpg" class="room-img">
-      <h4 @click="모달창열렸니 = true">{{products[0]}}</h4>
-      <p>{{price[0]}} 만원</p>
-      <button @click="increase(0)">허위매물신고</button> <span>신고수 : {{신고수[0]}}</span>
-    </div>
-
-  
-    <div>
-      <img src="./assets/room1.jpg" class="room-img">
-      <h4>{{products[1]}}</h4>
-      <p>{{price[1]}} 만원</p>
-      <button @click="increase(1)">허위매물신고</button> <span>신고수 : {{신고수[1]}}</span>
-    </div>
-
-    
-    <div>
-      <img src="./assets/room2.jpg" class="room-img">
-      <h4>{{products[2]}}</h4>
-      <p>{{price[2]}} 만원</p>
-      <button @click="increase(2)">허위매물신고</button> <span>신고수 : {{신고수[2]}}</span>
+    <div v-for="(a, i) in 6" :key="i">
+      <img :src="원룸들[i].image" class="room-img">
+      <h4>{{원룸들[i].title}}</h4>
+      <p>{{원룸들[i].price}} $</p>
     </div>
   </div>
 </template>
